f33019425b4a1efc0f6947e524d8de6cdd79b8b1
Austin Szema <aszema13@gmail.com>
2024-02-26 21:18:08 -0500

    Modified UI for the flying ability so that the meter only appears when its value is being changed

--- Assets/Scripts/PlayerScripts/SetFlightFuelValue.cs
+++ Assets/Scripts/PlayerScripts/SetFlightFuelValue.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,39 @@ public class SetFlightFuelValue : MonoBehaviour
 {
     [SerializeField] private Slider _flightFuelSlider;
     [SerializeField] private floatVariable _flightDuration;
-    [SerializeField] private float _maxFlightDuration; // Add this variable
+    
+    [SerializeField] private GameObject _flightSliderBackground;
+
+    private float _maxFlightDuration;
+
+    private void Start()
+    {
+        _maxFlightDuration = _flightDuration.Value;
+    }
 
     private void Update()
     {
         // Normalize flight duration between 0 and 1
         float normalizedValue = _flightDuration.Value / _maxFlightDuration;
-        
+
         // Ensure the value is clamped between 0 and 1
         normalizedValue = Mathf.Clamp01(normalizedValue);
-        
+
         // Set the slider value
         _flightFuelSlider.value = normalizedValue;
+
+        if (Input.GetKey(KeyCode.Space) && _flightDuration.Value < _maxFlightDuration)
+        {
+            _flightSliderBackground.SetActive(true);
+        }
+        else
+        {
+            _flightSliderBackground.SetActive(false);
+        }
+    }
+
+    public void EnableSliderVisual()
+    {
+        _flightSliderBackground.SetActive(true);
     }
 }
