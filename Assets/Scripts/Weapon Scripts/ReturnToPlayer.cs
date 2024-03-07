ae1268eb850a085efb94cb3e1cc363d4693a465f
Austin Szema <aszema13@gmail.com>
2024-03-07 18:03:27 -0500

    Updated the return to player functionality of the demon dice to account for physics updates

--- Assets/Scripts/Weapon Scripts/ReturnToPlayer.cs
+++ Assets/Scripts/Weapon Scripts/ReturnToPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,11 @@ using UnityEngine.Rendering;
 
 public class ReturnToPlayer : MonoBehaviour
 {
+    // Recalls all of the demon dice to the player when the player presses 'e'.
+    // Does not fully work properly at the moment with low frame rates
+    // If you shoot the demons really far away it takes multiple presses to get them to come back. Probably because their velocity is so high
+    // Mainly for testing purposes, likely won't be in the final build
+    
     [SerializeField] private Vector3Variable _playerPos;
 
     [SerializeField] private Rigidbody _rigidbody;
@@ -17,6 +23,19 @@ public class ReturnToPlayer : MonoBehaviour
         {
             _rigidbody.velocity = Vector3.zero;
             transform.position = _playerPos.Value + Vector3.up * 5f;
+            //Debug.Log(" press e on update");
         }
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _rigidbody.velocity = Vector3.zero;
+            transform.position = _playerPos.Value + Vector3.up * 5f;
+            //Debug.Log(" press e on fixedupdate");
+        }    
     }
 }
