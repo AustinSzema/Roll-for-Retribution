0606b0c5c7b59bf2fd28ba68376ebd7dc4ad03de
Austin Szema <aszema13@gmail.com>
2024-03-09 12:16:44 -0500

    Prototyped jumper enemy

---
+++ Assets/Scripts/Enemy Scripts/JumperFollowPlayer.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperFollowPlayer : MonoBehaviour
+{    
+    [SerializeField] private float _moveSpeed = 2f;
+
+    [SerializeField] private Vector3Variable _playerPosition;
+
+    [SerializeField] private boolVariable _gameIsPaused;
+
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [SerializeField] private int moveInterval;
+
+    [SerializeField] private float targetDistanceBehindPlayer;
+
+    private bool _charging;
+
+    private Vector3 goalPosition;
+
+    private void Awake()
+    {
+        _charging = false;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (_gameIsPaused.Value == false)
+        {
+            if (_charging == false)
+            {
+                StartCoroutine(Charge());
+            }
+            else
+            {
+                _rigidbody.position = Vector3.MoveTowards(transform.position, goalPosition, _moveSpeed * Time.deltaTime);
+            }
+        }
+    }
+    IEnumerator Charge()
+    {
+        _charging = true;
+      
+        float distToPlayer = Vector3.Distance(transform.position, _playerPosition.Value);
+        Vector3 moveDirection = (_playerPosition.Value - transform.position).normalized;
+        goalPosition = transform.position + moveDirection * (distToPlayer + targetDistanceBehindPlayer);
+        goalPosition.y = _playerPosition.Value.y;
+        yield return new WaitForSeconds(moveInterval);
+        _charging = false;
+    }
+}
