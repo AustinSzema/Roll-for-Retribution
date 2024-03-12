ec51d61be3152aa9fdc77675a8b1ea4b71dc3f1b
ramjsandal <ramjsandal@gmail.com>
2024-03-11 20:50:17 -0400

    added main menu

---
+++ Assets/Scripts/Dimension Scripts/ExitGame.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+  public void QuitGame()
+  {
+    Application.Quit();
+  } 
+}
