4786abb00c31db4b6bb382a4d3c05d15c5923ae6
ramjsandal <ramjsandal@gmail.com>
2024-04-01 20:49:05 -0400

    refactored old test code

--- Assets/Scripts/Shop Scripts/SkillPoints.cs
+++ Assets/Scripts/Shop Scripts/SkillPoints.cs
@@ -29,28 +29,15 @@ public class SkillPoints : MonoBehaviour
 
     private void Start()
     {
+        if (_thresholds.Length > SkillPointsToGainAtThreshold.Length)
+        {
+            throw new ArgumentException("Each threshold must have a corresponding number of skill points to gain");
+        }
         _gainAtThreshold = new Dictionary<int, int>();
-        _gainAtThreshold.Add(_thresholds[0], SkillPointsToGainAtThreshold[0]); // 1
-        _gainAtThreshold.Add(_thresholds[1], SkillPointsToGainAtThreshold[1]); // 2
-        _gainAtThreshold.Add(_thresholds[2], SkillPointsToGainAtThreshold[2]); // 3
-        _gainAtThreshold.Add(_thresholds[3], SkillPointsToGainAtThreshold[3]); // 4
-        _gainAtThreshold.Add(_thresholds[4], SkillPointsToGainAtThreshold[4]); // 5
-        _gainAtThreshold.Add(_thresholds[5], SkillPointsToGainAtThreshold[5]); // 6
-        _gainAtThreshold.Add(_thresholds[6], SkillPointsToGainAtThreshold[6]); // 7
-        _gainAtThreshold.Add(_thresholds[7], SkillPointsToGainAtThreshold[7]); // 8
-        _gainAtThreshold.Add(_thresholds[8], SkillPointsToGainAtThreshold[8]); // 9
-        _gainAtThreshold.Add(_thresholds[9], SkillPointsToGainAtThreshold[9]); // 10
-        _gainAtThreshold.Add(_thresholds[10], SkillPointsToGainAtThreshold[10]); // 11
-        _gainAtThreshold.Add(_thresholds[11], SkillPointsToGainAtThreshold[11]); // 12
-        _gainAtThreshold.Add(_thresholds[12], SkillPointsToGainAtThreshold[12]); // 13
-        _gainAtThreshold.Add(_thresholds[13], SkillPointsToGainAtThreshold[13]); // 14
-        _gainAtThreshold.Add(_thresholds[14], SkillPointsToGainAtThreshold[14]); // 15
-        _gainAtThreshold.Add(_thresholds[15], SkillPointsToGainAtThreshold[15]); // 16
-        _gainAtThreshold.Add(_thresholds[16], SkillPointsToGainAtThreshold[16]); // 17
-        _gainAtThreshold.Add(_thresholds[17], SkillPointsToGainAtThreshold[17]); // 18
-        _gainAtThreshold.Add(_thresholds[18], SkillPointsToGainAtThreshold[18]); // 19
-        _gainAtThreshold.Add(_thresholds[19], SkillPointsToGainAtThreshold[19]); // 20
-
+        for (int i = 0; i < _thresholds.Length; i++)
+        {
+            _gainAtThreshold.Add(_thresholds[i], SkillPointsToGainAtThreshold[i]);
+        }
         _thresholdIdx = 0;
     }
 
