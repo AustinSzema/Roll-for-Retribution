a37d508b4767a6de087e9e28776258894856e1ae
Austin Szema <aszema13@gmail.com>
2024-05-03 02:27:01 -0400

    Added gradients to some UI elements

--- Assets/Scripts/UI Scripts/PulseOutline.cs
+++ Assets/Scripts/UI Scripts/PulseOutline.cs
@@ -6,16 +6,22 @@ public class PulseOutline : MonoBehaviour
 {
     [SerializeField] private Outline _outline;
     [SerializeField] private float _pulseSpeed = 1.0f;
-    [SerializeField] private float _maxEffectDistance = 2.0f;
+    private float _maxEffectDistanceX = -15.0f;
+    private float _maxEffectDistanceY = 15.0f;
+
     private Vector2 _startingEffectDistance;
 
     private void Start()
     {
         _startingEffectDistance = _outline.effectDistance;
+        _maxEffectDistanceX = _outline.effectDistance.x;
+        _maxEffectDistanceY = _outline.effectDistance.y;
     }
 
     private void OnEnable()
     {
+        _maxEffectDistanceX = _outline.effectDistance.x;
+        _maxEffectDistanceY = _outline.effectDistance.y;
         _outline.effectDistance = Vector2.zero;        
     }
 
@@ -25,8 +31,8 @@ public class PulseOutline : MonoBehaviour
         float pulse = Mathf.Sin(Time.time * _pulseSpeed);
         
         // Map pulse value to effect distance within the range [0, maxEffectDistance]
-        float newX = Mathf.Lerp(0, _maxEffectDistance, pulse);
-        float newY = Mathf.Lerp(0, _maxEffectDistance, pulse);
+        float newX = Mathf.Lerp(0, _maxEffectDistanceX, pulse);
+        float newY = Mathf.Lerp(0, _maxEffectDistanceY, pulse);
 
         // Update outline effect distance
         _outline.effectDistance = new Vector2(newX, newY);
