ae0cd96c486ad6bbb7cfb44e37a6eb8d807a1b5f
Austin Szema <aszema13@gmail.com>
2024-05-03 01:33:35 -0400

    Changed levitate text to "Charging..."

--- Assets/Scripts/PlayerScripts/SetFlightFuelValue.cs
+++ Assets/Scripts/PlayerScripts/SetFlightFuelValue.cs
@@ -64,7 +64,7 @@ public class SetFlightFuelValue : MonoBehaviour
             }
             else
             {
-                _fuelTextMeshProUGUI.text = "Brewing...";
+                _fuelTextMeshProUGUI.text = "Charging...";
             }
 
 
