2c38dc2ddc25f6435557e38a549e47632ba2a018
Austin Szema <aszema16@gmail.com>
2024-05-05 15:15:35 -0400

    Fixed timer to display hours, minutes, seconds and milliseconds

--- Assets/Scripts/GameManagerFieldSetters/SetTimer.cs
+++ Assets/Scripts/GameManagerFieldSetters/SetTimer.cs
@@ -1,19 +1,23 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class SetTimer : MonoBehaviour
 {
-
-    
     [SerializeField] private TextMeshProUGUI _timerText;
 
     // Update is called once per frame
     void Update()
     {
-      //_timerText.text = Time.time.ToString("F2");
-      _timerText.text = Time.timeSinceLevelLoad.ToString("F2");
+        float timeInSeconds = Time.timeSinceLevelLoad;
+        TimeSpan timeSpan = TimeSpan.FromSeconds(timeInSeconds);
+
+        string timerString = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D3}",
+            timeSpan.Hours,
+            timeSpan.Minutes,
+            timeSpan.Seconds,
+            timeSpan.Milliseconds);
+
+        _timerText.text = timerString;
     }
 }
