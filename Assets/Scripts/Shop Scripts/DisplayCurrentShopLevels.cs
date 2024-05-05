ecae86a88954b7cfffb8ecee8be1c3980aac0812
Austin Szema <aszema16@gmail.com>
2024-05-05 14:48:00 -0400

    Refactored shop to display when an upgrade is sold out

--- Assets/Scripts/Shop Scripts/DisplayCurrentShopLevels.cs
+++ Assets/Scripts/Shop Scripts/DisplayCurrentShopLevels.cs
@@ -29,6 +29,8 @@ public class DisplayCurrentShopLevels : MonoBehaviour
 
     [SerializeField] private Shop shop;
 
+    private string _soldOut = "Sold Out";
+    
     private void Update()
     {
        UpdateFields();
@@ -40,13 +42,27 @@ public class DisplayCurrentShopLevels : MonoBehaviour
         pullForce.text = magnet._pullSpeed.ToString();
         demonQuantity.text = magnet.GetMagnetCount().ToString();
         demonWeight.text = Math.Round(magnet.GetDemonWeight(), 2).ToString();
-        pullForceCost.text = shop.CostToLevel(Shop.SkillsToLevel.PullForce).ToString();
-        demonQuantityCost.text = shop.CostToLevel(Shop.SkillsToLevel.DemonQuantity).ToString();
-        demonWeightCost.text = shop.CostToLevel(Shop.SkillsToLevel.DemonWeight).ToString();
-        // Debug.Log(pullForceCost.text);
-        // Debug.Log(diceQuantityCost.text);
-        // Debug.Log(diceWeight.text);
-        skillPoints.text = "Skill Points: "  + shop.CurrentSkillPoints().ToString();
+        
+        pullForce.text = magnet._pullSpeed.ToString();
+        demonQuantity.text = magnet.GetMagnetCount().ToString();
+        demonWeight.text = Math.Round(magnet.GetDemonWeight(), 2).ToString();
+
+        UpdateCostField(Shop.SkillsToLevel.PullForce, pullForceCost);
+        UpdateCostField(Shop.SkillsToLevel.DemonQuantity, demonQuantityCost);
+        UpdateCostField(Shop.SkillsToLevel.DemonWeight, demonWeightCost);
+
+        skillPoints.text = "Skill Points: " + shop.CurrentSkillPoints().ToString();
     }
     
+    private void UpdateCostField(Shop.SkillsToLevel skill, TextMeshProUGUI text)
+    {
+        if (shop.CostToLevel(skill) >= Int32.MaxValue) // TODO: should this use something other than Int32.MaxValue as a way to determine if there are no more upgrades
+        {
+            text.text = _soldOut;
+        }
+        else
+        {
+            text.text = shop.CostToLevel(skill).ToString();
+        }
+    }
 }
