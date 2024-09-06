b39324f464db11bc0aecf5f36355f42a51e78947
Austin Szema <aszema16@gmail.com>
2024-09-05 23:19:18 -0400

    Updated splash screen

--- Assets/Scripts/Player Scripts/TakeScreenshot.cs
+++ Assets/Scripts/Player Scripts/TakeScreenshot.cs
@@ -6,21 +6,37 @@ using UnityEngine;
 public class TakeScreenshot : MonoBehaviour
 {
     [SerializeField] private KeyCode inputKey;
-    
+
+    [SerializeField] private bool takeScreenshotOnStart = false;
+
+    private void Start()
+    {
+        if (takeScreenshotOnStart)
+        {
+            TakeAScreenshot();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(inputKey)) 
         {
-            string time = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
-            string fileName = "Assets/Screenshots/DemonTimeScreenshot_" + time + ".png";
-
-            Debug.Log("Screenshot saved as: " + fileName);
-            
-            ScreenCapture.CaptureScreenshot(fileName);
-            #if UNITY_EDITOR
-            AssetDatabase.Refresh();
-            #endif
+            TakeAScreenshot();
         }
+    }
+
+    private void TakeAScreenshot()
+    {
+        
+        string time = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+        string fileName = "Assets/Screenshots/DemonTimeScreenshot_" + time + ".png";
+
+        Debug.Log("Screenshot saved as: " + fileName);
+            
+        ScreenCapture.CaptureScreenshot(fileName);
+#if UNITY_EDITOR
+        AssetDatabase.Refresh();
+#endif
     }
 }
