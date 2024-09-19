73ab9a73b9a9c1e333b4e20d1af1b1afb2f673cb
Austin Szema <aszema13@gmail.com>
2024-09-19 14:36:41 -0400

    Updated UI

--- Assets/Scripts/GameManagerFieldSetters/SetTimer.cs
+++ Assets/Scripts/GameManagerFieldSetters/SetTimer.cs
@@ -1,42 +1,47 @@
 using System;
-using System.Collections.Generic;
-
 using TMPro;
 using UnityEngine;
 
 public class SetTimer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _timerText;
-    
-
     [SerializeField] private RoundSpawnConfig _roundSpawnConfig;
 
-    private float countdown = 1f;
-    
+    private float countdown;
+
     private void Start()
     {
-        countdown = _roundSpawnConfig.Value[GameManager.Instance.currentRound] // round 1
-            .Value[_roundSpawnConfig.Value[GameManager.Instance.currentRound].Value.Count-1].StartTime; // last element of round one
+        // Assuming StartTime is the time in seconds for the current round
+        countdown = _roundSpawnConfig.Value[GameManager.Instance.currentRound]
+            .Value[_roundSpawnConfig.Value[GameManager.Instance.currentRound].Value.Count - 1].StartTime;
 
-        _timerText.text = "" + countdown;
+        UpdateTimerText(countdown);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        /*float timeInSeconds = Time.timeSinceLevelLoad;
+        if (countdown > 0)
+        {
+            countdown -= Time.deltaTime;
+            UpdateTimerText(countdown);
+        }
+        else
+        {
+            countdown = 0;
+            UpdateTimerText(countdown); // Final update to ensure timer shows 0 when complete
+        }
+    }
+
+    private void UpdateTimerText(float timeInSeconds)
+    {
+        // Convert to TimeSpan to easily format minutes and seconds
         TimeSpan timeSpan = TimeSpan.FromSeconds(timeInSeconds);
 
-        string timerString = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D3}",
-            timeSpan.Hours,
+        // Display minutes and seconds (MM:SS format)
+        string timerString = string.Format("{0:D2}:{1:D2}",
             timeSpan.Minutes,
-            timeSpan.Seconds,
-            timeSpan.Milliseconds);
+            timeSpan.Seconds);
 
-        _timerText.text = timerString;*/
-
-
-        _timerText.text = (countdown - Time.timeSinceLevelLoad).ToString();
-
+        _timerText.text = timerString;
     }
 }
