f90b0d137b2a8c941a33564c930debffea043a7f
Austin Szema <aszema13@gmail.com>
2024-09-19 14:07:50 -0400

    made timer count down

--- Assets/Scripts/GameManagerFieldSetters/SetTimer.cs
+++ Assets/Scripts/GameManagerFieldSetters/SetTimer.cs
@@ -1,15 +1,30 @@
 using System;
+using System.Collections.Generic;
+
 using TMPro;
 using UnityEngine;
 
 public class SetTimer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _timerText;
+    
+
+    [SerializeField] private RoundSpawnConfig _roundSpawnConfig;
+
+    private float countdown = 1f;
+    
+    private void Start()
+    {
+        countdown = _roundSpawnConfig.Value[GameManager.Instance.currentRound] // round 1
+            .Value[_roundSpawnConfig.Value[GameManager.Instance.currentRound].Value.Count-1].StartTime; // last element of round one
+
+        _timerText.text = "" + countdown;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float timeInSeconds = Time.timeSinceLevelLoad;
+        /*float timeInSeconds = Time.timeSinceLevelLoad;
         TimeSpan timeSpan = TimeSpan.FromSeconds(timeInSeconds);
 
         string timerString = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D3}",
@@ -18,6 +33,10 @@ public class SetTimer : MonoBehaviour
             timeSpan.Seconds,
             timeSpan.Milliseconds);
 
-        _timerText.text = timerString;
+        _timerText.text = timerString;*/
+
+
+        _timerText.text = (countdown - Time.timeSinceLevelLoad).ToString();
+
     }
 }
--- Assets/Scripts/Player Scripts/CameraShake.cs
+++ Assets/Scripts/Player Scripts/CameraShake.cs
@@ -8,10 +8,12 @@ public class CameraShake : MonoBehaviour {
         float elapsed = 0.0f;
         while (elapsed < duration)
         {
-
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
-            transform.localPosition = new Vector3(x, y, originalPos.z);
+            if (!GameManager.Instance.gameIsPaused)
+            {
+                float x = Random.Range(-1f, 1f) * magnitude;
+                float y = Random.Range(-1f, 1f) * magnitude;
+                transform.localPosition = new Vector3(x, y, originalPos.z);
+            }
             elapsed += Time.deltaTime;
             yield return null;
         }
