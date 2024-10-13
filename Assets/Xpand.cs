a663b48f20cecf8f733eaea1a4ecf069cc6b7a57
Austin Szema <aszema16@gmail.com>
2024-10-12 22:30:28 -0400

    added expand weapon and made adding new weapons more flexible

---
+++ Assets/Xpand.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Xpand : Weapon
+{
+    [SerializeField] private float expandSize = 10f;
+
+    [SerializeField] private float rotationSpeed = 100f;
+    
+    // [SerializeField] private GameObject[] objectsToExpand;
+    //
+    // private List<Vector3> scales = new List<Vector3>();
+    //
+    // private void Start()
+    // {
+    //     foreach (GameObject obj in objectsToExpand)
+    //     {
+    //         scales.Add(obj.transform.localScale);
+    //     }
+    // }
+
+
+    private Vector3 startSize;
+    
+    private void Start()
+    {
+        startSize = transform.localScale;
+    }
+
+    public override void Shoot(Vector3 magnetForwardDirection)
+    {  
+        rb.AddForce(magnetForwardDirection* shootForce);
+    }
+    
+    protected override void OnCollisionEnter(Collision other)
+    {
+        base.OnCollisionEnter(other);
+        Expand(other.gameObject);
+    }
+
+    
+    
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+        Expand(other.gameObject);
+    }
+
+    private void Expand(GameObject other)
+    {
+        if (other.CompareTag(TagManager.groundTag))
+        {
+            transform.localScale = startSize * expandSize;
+            rb.velocity = Vector3.zero;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            /*
+            foreach (GameObject obj in objectsToExpand)
+            {
+                Vector3 scale = obj.transform.localScale;
+                obj.transform.localScale = new Vector3(scale.x, expandSize, scale.z);
+
+            }
+        */
+        }
+    }
+
+    
+    public override void Attract(Vector3 magnetPosition)
+    {
+        base.Attract(magnetPosition);
+        rb.constraints = RigidbodyConstraints.None;
+        transform.localScale = Vector3.one;
+        
+        /*for (int i = 0; i < objectsToExpand.Length; i++)
+        {
+            objectsToExpand[i].transform.localScale = scales[i];
+        }*/
+        
+        Quaternion targetRotation = Quaternion.Euler(0, 0, 0);
+
+        // Smoothly rotate towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+    
+}
+
