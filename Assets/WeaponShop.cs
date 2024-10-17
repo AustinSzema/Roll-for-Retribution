701a0ccf8ab3a21e22d9b2f7e431c2e923e14277
Austin Szema <aszema13@gmail.com>
2024-10-17 16:56:20 -0400

    prototyped new weapon round system

--- Assets/WeaponShop.cs
+++ Assets/WeaponShop.cs
@@ -2,29 +2,50 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class WeaponShop : MonoBehaviour
 {
     [SerializeField] private WeaponList weaponList;
 
     [Space] [SerializeField] private List<Image> buttons = new List<Image>();
+    [Space] [SerializeField] private List<TextMeshProUGUI> descriptions = new List<TextMeshProUGUI>();
 
-    private void Start()
-    {
-        if (buttons == null)
-        {
-            buttons = GetComponentsInChildren<Image>().ToList();
-        }
-    }
+    [SerializeField] private GameObject loadingText;
+    
+    private List<GameObject> internalWeaponsList = new List<GameObject>();
 
     private void OnEnable()
     {
+        internalWeaponsList = weaponList.weaponList;
+        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
         for (int i = 0; i < buttons.Count; i++)
         {
-            Weapon weapon = weaponList.weaponList[i].GetComponent<Weapon>() ?? weaponList.weaponList[i].GetComponentInChildren<Weapon>();
+            int random = Random.Range(0, internalWeaponsList.Count-1);
+            Weapon weapon = internalWeaponsList[random].GetComponent<Weapon>() ?? internalWeaponsList[random].GetComponentInChildren<Weapon>();
             buttons[i].sprite = weapon.weaponUISprite;
+            descriptions[i].text = weapon.weaponDescription;
+            internalWeaponsList.Remove(internalWeaponsList[random]);
         }
+    }
+
+    private void OnDisable()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+   
+    }
+
+    public void NextScene()
+    {
+        loadingText.SetActive(true);
+        GameManager.Instance.currentRound++;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
