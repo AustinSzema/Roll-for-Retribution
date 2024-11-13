c031e3bbe604be4f9d3b3115b568feafbf7a0290
sadprofeso <106486391+sadprofeso@users.noreply.github.com>
2024-11-13 09:32:28 -0500

    increased skull push speed by 25%, fixed skull ring shoot so it fires in a consistent direction

--- Assets/Scripts/Weapon Scripts/SkullRing.cs
+++ Assets/Scripts/Weapon Scripts/SkullRing.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class SkullRing : Weapon
 {
-
+    public override void Shoot(Vector3 magnetForwardDirection)
+    {
+        rb.AddForce(shootForce * magnetForwardDirection);
+    }
 }
