8e75d7a1b20153a17b142ffccf0d15099ac038ed
Austin Szema <aszema13@gmail.com>
2025-03-04 18:48:26 -0500

    eyeball

---
+++ Assets/Eyeball.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Eyeball : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Quaternion lookRotation = Quaternion.identity;
+
+    [SerializeField] private float rotationSpeed = 20f;
+    
+    private IEnumerator Start()
+    {
+        while(true)
+      {
+          lookRotation = Quaternion.Euler(Random.onUnitSphere * 100f);      
+          yield return new WaitForSeconds(Random.Range(0.5f, 2f));
+          }
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+}
