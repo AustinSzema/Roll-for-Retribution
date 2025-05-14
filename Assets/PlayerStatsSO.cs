5c43d4204300849fb030d752bc7401bfced68fcf
Austin Szema <aszema16@gmail.com>
2025-05-14 02:23:15 -0400

    particles

--- Assets/PlayerStatsSO.cs
+++ Assets/PlayerStatsSO.cs
@@ -5,7 +5,7 @@ public class PlayerStatsSO : ScriptableObject
 {
     
     [Header("Ground Movement")]
-    public float moveSpeed = 30f;
+    public float moveSpeed = 100f;
     public float groundDrag = 5f;
     [Space]
     [Tooltip("Unused")] public float walkSpeed;
@@ -16,29 +16,29 @@ public class PlayerStatsSO : ScriptableObject
     public float jumpCooldown = 0.25f;
     public float airMultiplier = 1.5f;
     [Space]
-    public float groundPoundForce = -60f;
-    public float groundPoundCooldown = 0.25f;
+    public float groundPoundForce = -240f;
+    public float groundPoundCooldown = 1f;
     [Space]
     //public float gravityMultiplier = 2f;
 
     [Space]
     public float fallGravityMultiplier = 5f;
-    public float lowJumpGravityMultiplier = 2f;
-    public float baseGravity = -9.81f;
+    public float lowJumpGravityMultiplier = 3f;
+    public float baseGravity = -19.62f;
 
     
     
-    [Header("Levitate Ability")] public float _maxFlightDuration = 10;
-    [SerializeField] public float _fuelDecrementAmount = 1;
-    [SerializeField] public float _fuelRechargeAmount = 1;
-    public float _fuelPenaltyThreshold = 1;
+    [Header("Levitate Ability")] public float _maxFlightDuration = 150;
+    [SerializeField] public float _fuelDecrementAmount = 0.75f;
+    [SerializeField] public float _fuelRechargeAmount = 0.1f;
+    public float _fuelPenaltyThreshold = 125;
     [Tooltip("Sets the Y value of player's velocity")] [SerializeField]
-    public float _flightForce = 30f;
+    public float _flightForce = 80f;
 
 
     
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
-    public KeyCode groundPoundKey = KeyCode.LeftControl;
+    public KeyCode groundPoundKey = KeyCode.LeftShift;
 
 }
